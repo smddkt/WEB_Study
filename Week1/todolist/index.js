d0980baf27121da039b2e3a5d38d43ad34519b7d
SANGEUN LEE <sangeun0205@naver.com>
2024-04-11 22:43:57 +0900

    todo_list

--- Week1/todolist/index.js
+++ Week1/todolist/index.js
@@ -1,84 +1,42 @@
-const input = document.querySelector('input')
+const Input = document.getElementById('input');
+const item1 = document.querySelector('.item1');
+const item2 = document.querySelector('.item2');
+
+Input.addEventListener('keyup', function(event){
+  addList(event);
+});
 
 
-input.addEventListener('keyup', () => {
-    whatTodo.innerHTML = input
-})
-
-
-
-
-function addList()  {
-  
-    // 1. 추가할 값을 input창에서 읽어온다
-    const input = document.getElementById('input').value;
-    
-    // 2. 추가할 li element 생성
-    // 2-1. 추가할 li element 생성
-    const li = document.createElement("li");
-    
-    // 2-2. li에 id 속성 추가 
-    li.setAttribute('id',addValue);
-    
-    // 2-3. li에 text node 추가 
-    const textNode = document.createTextNode(input);
-    li.appendChild(textNode);
-    
-    // 3. 생성된 li를 ul에 추가
-    document
-      .getElementById('fruits')
-      .appendChild(li);
+item1.addEventListener("click", function(event){
+  const target = event.target;
+  if(target.tagName === "BUTTON"){
+    const parentUl = target.parentNode;
+    parentUl.remove();
+    item2.appendChild(parentUl);
+    target.innerText = "삭제";
   }
-  
+});
 
 
-
-  function removeItem()  {
-  
-    // 1. <ul> element 선택
-    const ul = document
-      .getElementById('fruits');
-    
-    // 2. <li> 목록 선택
-    const items = ul.getElementsByTagName('li');
-    
-    // 3. <li> 목록 중 첫번째 item 삭제
-    if(items.length > 0)  {
-      items[0].remove();
-    }
-    
+item2.addEventListener("click", function(event){
+  const target = event.target;
+  if(target.tagName === "BUTTON"){
+    const parentUl = target.parentNode;
+    parentUl.remove();
   }
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function add_list(event){
-//     event.preventDefault();
-//     const code = event.code;
-//     const input = document.getElementById("input").value;
-//     const whatTodo = document.getElementById("whatTodo");
-//     const todoInput = document.querySelector("#input");
-
-//     if (code === 'Enter'){
-//     whatTodo.innerHTML = input
-//     todoInput.value = ' ';
-//     }
-// }
-
-
-// todoInput.addEventListener('keyup', function(event){
-//         add_list(event);
-// });
+function addList(event) {
+  if(event.key ==="Enter"){
+  event.preventDefault();
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  const button1 = document.createElement("button");
+  button1.innerText = '완료';
+  li.textContent = Input.value;
+  ul.appendChild(li);
+  ul.appendChild(button1);
+  item1.appendChild(ul);
+  Input.value = '';
+}}
