34741f2256d14f63f108130fff837100ad277a5e
Sangeun <sangeun0205@naver.com>
2024-07-02 08:29:33 +0900

    24.07.02

---
+++ JS_tutorial/typescript.js
@@ -0,0 +1,5 @@
+let 이름 = ['Lee', 'park'];
+let 이름 = 'Lee';
+let 이름 = { 이름: 'Lee' };
+let 이름 = 'Lee';
+let 이름 = 90090;
---
+++ JS_tutorial/typescript.ts
@@ -0,0 +1,7 @@
+let 이름 :string[ ] = ['Lee', 'park'];
+let 이름 :string = 'Lee';
+let 이름 :{이름 : string} = { 이름 : 'Lee' };
+
+
+let 이름 :string | number = 'Lee';
+let 이름 :string | number = 90090
