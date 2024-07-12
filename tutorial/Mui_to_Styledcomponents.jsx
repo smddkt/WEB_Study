22e54c645cdbbb9b43c91898df7ee93f4164cb7a
Sangeun <sangeun0205@naver.com>
2024-07-12 23:02:11 +0900

    16times... 24.07.12

---
+++ tutorial/Mui_to_Styledcomponents.jsx
@@ -0,0 +1,402 @@
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.DISPLAY1 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 80,
+        lineHeight: "96px",
+        fontWeight: 700,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//Styled-Components
+const Display1 = styled.div`
+  font-family: "Pretendard";
+  font-size: 80px;
+  line-height: 96px;
+  font-weight: 700;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.DISPLAY2 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 52,
+        lineHeight: "64px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//Styled-Components
+const Display2 = styled.div`
+  font-family: "Pretendard";
+  font-size: 52px;
+  line-height: 64px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING1 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 42,
+        lineHeight: "52px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Heading1 = styled.div`
+  font-family: "Pretendard";
+  font-size: 42px;
+  line-height: 52px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING2 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 36,
+        lineHeight: "44px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Heading2 = styled.div`
+  font-family: "Pretendard";
+  font-size: 36px;
+  line-height: 44px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING3 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 32,
+        lineHeight: "40px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Heading3 = styled.div`
+  font-family: "Pretendard";
+  font-size: 32px;
+  line-height: 40px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING4 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 28,
+        lineHeight: "36px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Heading4 = Styled.div`
+  font-family: 'Pretendard', 
+  font-size: 28px;
+  line-height: 36px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING5 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 24,
+        lineHeight: "32px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Heading5 = styled.div`
+  font-family: "Pretendard";
+  font-size: 24px;
+  line-height: 32px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.HEADING6 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 20,
+        lineHeight: "28px",
+        fontWeight: 600,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComoponents
+const Heading6 = styled.div`
+  font-family: "Pretendard";
+  font-size: 20px;
+  line-height: 28px;
+  font-weight: 600;
+  color: ${(props) => props.color};
+`;
+
+//MUI
+{
+  typeof type === "string" && type === FTypographyType.LABEL1 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 18,
+        lineHeight: "24px",
+        fontWeight: 500,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+//StyledComponents
+const Label1 = styled.div`
+  font-family: "Pretendard";
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.LABEL2 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 16,
+        lineHeight: "20px",
+        fontWeight: 500,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+
+const Label2 = styled.div`
+  font-family: "Pretendard";
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.LABEL3 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 14,
+        lineHeight: "16px",
+        fontWeight: 500,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Label3 = styled.div`
+  font-family: "Pretendard";
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.LABEL4 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 12,
+        lineHeight: "16px",
+        fontWeight: 500,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Label4 = styled.div`
+  font-family: "Pretendard";
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 500;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.BODY1 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 18,
+        lineHeight: "28px",
+        fontWeight: 400,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Body1 = styled.div`
+  font-family: "Pretendard";
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 400;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.BODY2 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 16,
+        lineHeight: "24px",
+        fontWeight: 400,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Body2 = styled.div`
+  font-family: "Pretendard";
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.BODY3 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 14,
+        lineHeight: "20px",
+        fontWeight: 400,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Body3 = styled.div`
+  font-family: "Pretendard";
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  color: ${(props) => props.color};
+`;
+
+{
+  typeof type === "string" && type === FTypographyType.BODY4 && (
+    <Typography
+      sx={{
+        fontFamily: "Pretendard",
+        fontSize: 12,
+        lineHeight: "20px",
+        fontWeight: 400,
+        color: color,
+        ...sx,
+      }}
+    >
+      {text}
+    </Typography>
+  );
+}
+const Body4 = styled.div`
+  font-family: "Pretendard";
+  font-size: 12px;
+  line-height: 20px;
+  font-weight: 400;
+  color: ${(props) => props.color};
+`;
