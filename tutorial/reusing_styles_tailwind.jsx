f97fee7964686f83bfab232e53bbce70ce7af921
Sangeun <sangeun0205@naver.com>
2024-07-14 20:38:53 +0900

    24.07.14

---
+++ tutorial/reusing_styles_tailwind.jsx
@@ -0,0 +1,69 @@
+<>
+<div>
+  <div class="flex items-center space-x-2 text-base">
+    <h4 class="font-semibold text-slate-900">Contributors</h4>
+    <span class="rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700">204</span>
+  </div>
+  <div class="mt-3 flex -space-x-2 overflow-hidden">
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="..." alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="..." alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="..." alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="..." alt=""/>
+    <img class="inline-block h-12 w-12 rounded-full ring-2 ring-white" src="..." alt=""/>
+  </div>
+  <div class="mt-3 text-sm font-medium">
+    <a href="#" class="text-blue-500">+ 198 others</a>
+  </div>
+</div>
+
+//이렇게 반복되는 utility class 처리하기
+
+
+
+//1. 반복문
+<nav className="flex sm:justify-center space-x-4">
+  {[
+    ['Home', '/dashboard'],
+    ['Team', '/team'],
+    ['Projects', '/projects'],
+    ['Reports', '/reports'],
+  ].map(([title, url]) => (
+    <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+  ))}
+</nav>
+
+//2.컴포넌트 추출
+
+function Notification({ imageUrl, imageAlt, title, message }) {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="shrink-0">
+        <img className="h-12 w-12" src={imageUrl.src} alt={imageAlt} />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">{title}</div>
+        <p className="text-slate-500">{message}</p>
+      </div>
+    </div>
+  );
+}
+
+
+//3. @apply를 사용하여 클래스 추출
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer components {
+  .btn-primary {
+    @apply py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75;
+  }
+}
+
+
+
+
+
+
+</>
