5051d7629c535e134f0b481d7696a8f81a4bfbf1
Sang Eun Lee <sangeun0205@naver.com>
2024-08-06 23:52:41 +0900

    24.08.06

--- tutorial/swr.tsx
+++ tutorial/swr.tsx
@@ -17,3 +17,14 @@ export default function Page() {
 }
 
 
+
+
+
+
+function Profile() {
+  const { data, error, isLoading } = useSWR('/api/user', fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  return <div>hello {data.name}!</div>
+}
